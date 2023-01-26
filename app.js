2293b5269f4a968b2c06bc0953ab0e092f3cbe73
Nazhat Afza Zain <nazhatafza1@gmail.com>
2023-01-26 19:02:04 +0700

    modularization

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const coffee = require("./lib/coffee");
+
 const firstName = process.argv[2];
 const lastName = process.argv[3];
 
 console.log(`Hello Mr. ${firstName} ${lastName}`);
+
+// menjalankan coffee.js
+
+console.log(coffee)
