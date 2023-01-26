8956a68288452232f68062dee57ba07cdda8e391
Nazhat Afza Zain <nazhatafza1@gmail.com>
2023-01-26 19:06:28 +0700

    module

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const coffee = require("./lib/coffee");
+const user = require("./lib/user");
+const http = require('http');
 
 const firstName = process.argv[2];
 const lastName = process.argv[3];
@@ -8,3 +10,6 @@ console.log(`Hello Mr. ${firstName} ${lastName}`);
 // menjalankan coffee.js
 
 console.log(coffee)
+
+// menjalankan user.js
+console.log(user)
