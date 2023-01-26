5692af7767ad85fdb387923af56793d5513ebfe3
Nazhat Afza Zain <nazhatafza1@gmail.com>
2023-01-26 14:23:47 +0700

    process object pusing

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const message = (name) => {
-    console.log(`hello ${name}`);
+const initialMemoryUsage = process.memoryUsage().heapused;
+const yourName = process.argv[2];
+const environment = process.env.NODE_ENV;
+
+for (let i = 0; i <= 10000; i++) {
+    // proses looping ini akan membuat penggunaan memori naik
 }
 
-message('JavaScript');
+const currentMemoryUsage = process.memoryUsage().heapused;
+console.log(`hai ${yourName}`);
+console.log(`Mode Environment: ${environment}`);
+console.log(`penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
